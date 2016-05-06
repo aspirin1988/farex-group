66db1e24176a6059a321efabde8e081507870642
GoshanFloyd <egorkulik9696@gmail.com>
2016-05-06 15:54:40 +0600

    123123123

--- wp-content/themes/farex-group/template-parts/content-index.php
+++ wp-content/themes/farex-group/template-parts/content-index.php
@@ -14,7 +14,7 @@
 				<div class="item <?php if (!$key) echo 'active';  ?>">
 					<img class="carousel-item" src="<?=$val['path'];?>" alt="content1">
 					<div class="carousel-caption custom-caption">
-						<p>&#8220; <?=$val['img_title'];?> &#8221;</p>
+						<p class="slider-caption-font">&#8220; <?=$val['img_title'];?> &#8221;</p>
 						<p>
 							<?php if (!$key) : ?><img class="image-caption" src="<?php bloginfo('template_directory') ?>/public/img/fanera.png" alt="fanera"><?php endif;?>
 						<p class="text-sub-caption">
